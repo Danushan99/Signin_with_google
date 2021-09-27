ad0949b3cea3e6b02f438467eaccba7856519502
danushan99 <danuthasan08@gmail.com>
2021-09-27 13:34:28 +0530

    conecting db

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/state_manager.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googlelogin/login_controler.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final controller = Get.put(LoginController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SignInButton(
-                Buttons.Google,
-                onPressed: () {
-                  GoogleSignIn().signIn();
-                },
-              ),
-            ]),
+        child: Obx(() {
+          if (controller.googleAccount.value == null)
+            return buildLoginButton();
+          else
+            return buildProfileView();
+        }),
       ),
+    );
+  }
+
+  Column buildProfileView() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CircleAvatar(
+          backgroundImage:
+              Image.network(controller.googleAccount.value?.photoUrl ?? '')
+                  .image,
+          radius: 80,
+        ),
+        Text(controller.googleAccount.value?.displayName ?? ''),
+        Text(controller.googleAccount.value?.email ?? ''),
+      ],
+    );
+  }
+
+  FloatingActionButton buildLoginButton() {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        controller.login();
+      },
+      label: Text('Sign in with Google'),
     );
   }
 }
