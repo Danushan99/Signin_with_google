79ea5892a60987f68cec5252ab6632c651c17772
danushan99 <danuthasan08@gmail.com>
2021-09-27 12:42:08 +0530

    ios configration

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_builder.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      appBar: AppBar(title: const Text('Login')),
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               SignInButton(
                 Buttons.Google,
-                onPressed: () {},
+                onPressed: () {
+                  GoogleSignIn().signIn();
+                },
               ),
             ]),
       ),
